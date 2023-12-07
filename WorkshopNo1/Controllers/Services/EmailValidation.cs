55e51df1194c350c053c964ba68f5c83b69bc847
sxergiu <sxergiu@gmail.com>
2023-12-07 20:19:36 +0200

    Added email validation [hopefully]

---
+++ WorkshopNo1/Controllers/Services/EmailValidation.cs
@@ -0,0 +1,23 @@
+using System.Text.RegularExpressions;
+
+namespace WorkshopNo1.Controllers.Services;
+
+    public abstract class EmailValidation
+    {
+        public static bool IsValid(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+            try
+            {
+                var pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"; 
+                var regex = new Regex(pattern);
+                return regex.IsMatch(email);
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+
+    }
